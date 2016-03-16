8c55a7200cce94ceba00c56930adb57ffb2c56c0
Take Mouri <takehiromouri@gmail.com>
2016-03-15 23:49:02 -0400

    js fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,11 +11,11 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery_ujs
 //= require bootstrap-sprockets
 //= require moment
 //= require bootstrap-datetimepicker
 //= require pickers
-//= require jquery_ujs
 //= require turbolinks
 //= require_tree .
 
