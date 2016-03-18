3aed977c75f4067e523ed9ec85b0b192526f4db0
Take Mouri <takehiromouri@gmail.com>
2016-03-18 14:09:47 -0400

    view updates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def fullname(user)
     "#{user.first_name} #{user.last_name}"
   end
+
+  def display_datetime(datetime)
+    datetime.strftime("%m/%d/%Y %l:%M %p")
+  end
 end
--- app/helpers/appointment_requests_helper.rb
+++ app/helpers/appointment_requests_helper.rb
@@ -1,2 +1,22 @@
 module AppointmentRequestsHelper
+  def status(incoming_request)
+    if incoming_request.approved 
+      "Approved" 
+    elsif incoming_request.declined 
+      "Declined" 
+    else 
+      "Waiting" 
+    end
+  end
+
+  def status_icon(incoming_request)
+    if incoming_request.approved 
+      '<i class="fa fa-check-circle" style="color:#05CE05;"></i>'.html_safe
+    elsif incoming_request.declined 
+      '<i class="fa fa-times-circle" style="color:#D9534F"></i>'.html_safe
+    else 
+      '<i class="fa fa-hourglass-half"></i>'.html_safe
+    end
+
+  end
 end
