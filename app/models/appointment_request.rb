ccf89254e31596bb70b7cb36ed5b5d0f77a6f9b5
Take Mouri <takehiromouri@gmail.com>
2016-03-26 11:00:35 -0400

    stripe api keys

--- app/models/appointment_request.rb
+++ app/models/appointment_request.rb
@@ -8,6 +8,30 @@ class AppointmentRequest < ActiveRecord::Base
   validates :company_id, :user_id, :start_time, :end_time, presence: true
   validate :start_must_be_before_end_time
 
+  before_save :convert_to_datetime
+
+  def sched_date_field
+    sched_time.strftime("%d/%m/%Y") if start_time.present?
+  end 
+
+  def sched_time_field
+    sched_time.strftime("%I:%M%p") if start_time.present?
+  end
+
+  def sched_date_field=(date)
+    # Change back to datetime friendly format
+    @sched_date_field = Date.parse(date).strftime("%Y-%m-%d")
+  end
+
+  def sched_time_field=(time)
+    # Change back to datetime friendly format
+    @sched_time_field = Time.parse(time).strftime("%H:%M:%S")
+  end
+
+  def convert_to_datetime
+    self.start_time = DateTime.parse("#{@sched_date_field} #{@sched_time_field}")
+  end
+
   private
 
   def start_must_be_before_end_time
