6dd4afd5e43061aec7251ea10efb50c28e0d00b5
Take Mouri <takehiromouri@gmail.com>
2016-03-26 13:34:30 -0400

    add date validation

--- app/controllers/appointment_requests_controller.rb
+++ app/controllers/appointment_requests_controller.rb
@@ -1,5 +1,6 @@
 class AppointmentRequestsController < ApplicationController
   before_action :authenticate_user!
+  before_action :check_invalid_date, only: :create
 
   def index
     @appointment_requests = current_user.appointment_requests.order(updated_at: :desc)
@@ -20,7 +21,9 @@ class AppointmentRequestsController < ApplicationController
   end
 
   def create
-    @appointment_request = AppointmentRequest.create(appointment_request_params, end_time: DateTime.parse(params[:appointment_request][:start_time]) + 30.minutes)
+    params[:appointment_request][:end_time] = (DateTime.parse(params[:appointment_request][:start_time]) + 30.minutes).strftime('%Y-%m-%d %k:%M')
+    binding.pry
+    @appointment_request = AppointmentRequest.create(appointment_request_params)
     if @appointment_request.save
       params[:items].each{ |item| @appointment_request.appointment_items.create(item_id: item) } # create appointment item objects for each item
       redirect_to appointment_requests_path
@@ -31,6 +34,15 @@ class AppointmentRequestsController < ApplicationController
   end
 
   private
+
+  def check_invalid_date
+    begin
+      DateTime.parse(params[:appointment_request][:start_time])
+    rescue
+      flash[:danger] = "Invalid date"
+      redirect_to :back
+    end
+  end
 
   def appointment_request_params
     params.require(:appointment_request).permit(:company_id, :user_id, :start_time, :end_time, :approved, :declined, :sched_date_field, :sched_time_field)
