60a22627d99eb7de3861f078e87d5e2559558183
Take Mouri <takehiromouri@gmail.com>
2016-03-30 11:48:48 -0400

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,8 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => 'aluxurelife.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.office365.com',
