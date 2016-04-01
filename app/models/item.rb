ed74705c7f948185d9905ed58f8eda7d9f984687
Take Mouri <takehiromouri@gmail.com>
2016-04-01 16:51:20 -0400

    revise item form

--- app/models/item.rb
+++ app/models/item.rb
@@ -45,7 +45,7 @@ class Item < ActiveRecord::Base
 
   MENS_INSEAM_LENGTH = ["", "Short", "Regular", "Long"]
 
-  MENS_JACKET_SIZES = ["38", "40", "42", "44", "46", "48", "50", "52", "54", "56"]
+  MENS_JACKET_SIZES = ["", "38", "40", "42", "44", "46", "48", "50", "52", "54", "56"]
 
   MENS_SUIT_LENGTH = ["", "Short", "Regular", "Long", "X-Long"]
 
