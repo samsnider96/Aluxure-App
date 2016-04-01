807136594bea32befe897ddaf3d53a4a3c9f70ed
Take Mouri <takehiromouri@gmail.com>
2016-04-01 16:10:44 -0400

    use sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,12 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.office365.com',
+  :address        => 'smtp.sendgrid.net',
   :port           => '587',
-  :authentication => :login,
-  :user_name      => ENV['SMTP_USERNAME'],
-  :password       => ENV['SMTP_PASSWORD'],
-  :domain         => 'aluxurelife.com',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
   :enable_starttls_auto => true
- }
+}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,13 +1,13 @@
 Rails.application.configure do
-  ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'aluxurelife.com',
-    :authentication => :plain,
-  }
-  ActionMailer::Base.delivery_method = :smtp
+  # ActionMailer::Base.smtp_settings = {
+  #   :port           => ENV['MAILGUN_SMTP_PORT'],
+  #   :address        => ENV['MAILGUN_SMTP_SERVER'],
+  #   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  #   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  #   :domain         => 'aluxurelife.com',
+  #   :authentication => :plain,
+  # }
+  # ActionMailer::Base.delivery_method = :smtp
 
   config.action_mailer.default_url_options = { :host => 'aluxurelife.com' }
   # config.action_mailer.delivery_method = :smtp
