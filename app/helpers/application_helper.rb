77cc07c5f69f6e8ed372c8e4f43b43e9460f1061
Take Mouri <takehiromouri@gmail.com>
2016-04-02 16:45:08 -0400

    better item status radio buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,16 @@
 module ApplicationHelper
+
   def fullname(user)
     "#{user.first_name} #{user.last_name}"
   end
 
   def display_datetime(datetime)
     datetime.strftime("%m/%d/%Y %l:%M %p")
+  end
+end
+
+class String
+  def to_empty
+    "hi"
   end
 end
