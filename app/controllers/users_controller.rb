e6aa90684f47f02e282ebcb0e5be6be6c0bb5b9c
Take Mouri <takehiromouri@gmail.com>
2016-04-06 20:42:55 -0400

    css fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
+    @user = User.find(params[:id])
     @items = Item.where(user_id: params[:id]).order(updated_at: :desc).page(params[:page]).per(5)
   end
 end
